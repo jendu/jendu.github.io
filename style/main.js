0c8d282434d325c689e96b9628aba5b7ad8fd858
jendu <jenniferdu97@gmail.com>
2020-08-19 14:41:48 +0100

    Added function to click on image to open full size in new tab
    +Added scroll behaviour for nice jumping transition when using same-page links
    +Reorganised CSS too :-)
    +Moved age js back to main.js

--- style/main.js
+++ style/main.js
@@ -27,5 +27,33 @@ function copyright(){
   document.getElementById("copyright").innerHTML+=year;
 }
 
+function enlarge(){
+  var images=document.getElementById("fill").getElementsByTagName('img');
+  for(var i=0;i<images.length;i++){
+    images[i].addEventListener('click',function(){openImgInNewWindow(this)},false);
+  }
+}
+function openImgInNewWindow(img) {
+   var imgWindow=window.open();
+   imgWindow.document.write("<html><head><title>Jennifer Du – Full-size Image</title></head><body><img src="+img.src+" alt='Full-size image'></body></html>");
+}
+
+function age(){
+  var now=new Date();
+  var birthDate = new Date("12/26/1997");
+  var age=now.getFullYear()-birthDate.getFullYear();
+  var m=now.getMonth()-birthDate.getMonth();
+  if(m<0 || (m==0 && now.getDate()<birthDate.getDate()) ){
+    age=age-1;
+  }
+  document.getElementsByClassName("age")[0].innerHTML=age;
+}
+
 updateClock();
 copyright();
+if(document.getElementById("fill").getElementsByTagName('img')){
+  enlarge();
+}
+if(document.getElementsByClassName("age").length>0){
+  age();
+}
